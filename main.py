6d257cb07fa53b4defc57e880b4277606619d465
= <zaplatarivan@gmail.com>
2021-10-12 23:26:09 -0500

    Simplified how we scrape the data from website, now in a list vs datafrane

--- main.py
+++ main.py
@@ -7,33 +7,33 @@ import json
 proxies_url = "https://www.google-proxy.net/"
 
 def scrape_proxies(proxies_url):
+	print("------------------------------------------")
+	print("|           [Scraping Proxies]           |")
+	print("------------------------------------------")
 	html = requests.get(proxies_url)
 	content = BeautifulSoup(html.text, 'lxml')
 	table = content.find('table')
 	rows = table.findAll('tr')
-	headers = [header.text for header in rows[0]]
-	results = []
+	proxies = []
 	for row in rows:
-		results.append([data.text for data in row.findAll('td')])
-	data = pd.DataFrame(results[1::], columns=headers)
-	return data
+		all_data = [data.text for data in row.findAll('td', limit=2)]
+		if(len(all_data) == 2):
+			parser_dict = {"IP": None, "Port": None}
+			parser_dict["IP"] = (all_data[0])
+			parser_dict["Port"] = (all_data[1])
+			proxies.append("http://" + parser_dict["IP"] + ":" + parser_dict["Port"])
+	return proxies
 
-def rotate(proxy, port):
-	parsed_proxy = "http://" + proxy + ":" + port
+def rotate(proxy):
 	try:
-		r = requests.get("https://httpbin.org/ip", proxies={"http": parsed_proxy, "https": parsed_proxy}, timeout=2)
+		r = requests.get("https://httpbin.org/ip", proxies={"http": proxy, "https": proxy}, timeout=2)
 		print(r.json(), " - worked!")
+		print("-----------------------------------------")
+
 	except:
 		pass
 
-
-def rotate_list(proxy_list):
-	for proxy, port in zip(proxy_list['IP Address'], proxy_list["Port"]):
-		rotate(proxy, port)
-
 proxies = scrape_proxies(proxies_url)
-proxies_ip = proxies['IP Address'].tolist()
-proxies_port = proxies["Port"].tolist()
 with concurrent.futures.ThreadPoolExecutor() as executor:
-	executor.map(rotate, proxies_ip, proxies_port)
+	executor.map(rotate, proxies)
 
